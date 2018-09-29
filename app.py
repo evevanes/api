f8747afba41bed473ba4175588e5cf4d4c5cc4f4
Everlin Wanjiku <everlinwanjiku@gmail.com>
2018-09-29 14:06:52 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,32 @@
+#!flask/Source/python
+import mysql.connector
+from mysql.connector import errorcode
+from flask import Flask
+
+
+
+try:
+  
+ connection = mysql.connector.connect(
+	host = 'localhost',
+	user = '',
+	password = '',
+	database = 'todo_list')
+except mysql.connector.Error as err:
+  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+    print("Something is wrong with your user name or password")
+  elif err.errno == errorcode.ER_BAD_DB_ERROR:
+    print("Database does not exist")
+  else:
+    print(err)
+else:
+  connection.close()
+
+app = Flask('app')
+
+@app.route('/')
+def index():
+	return 'Hello world'
+
+if __name__== '__main__':
+	app.run(debug=True)
